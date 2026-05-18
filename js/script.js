a1915859a128e9f232af845d2aebd5eb6eb18845
Mike <fiblamikel@gmail.com>
2026-05-18 09:27:09 +0200

    new invitation design

--- js/script.js
+++ js/script.js
@@ -2,14 +2,16 @@ const openBtn = document.querySelector('.icon-open-menu .ico-menu')
 const closeBtn = document.querySelector('.nav-menu .ico-close')
 const navMenu = document.querySelector('.header-menu')
 
-openBtn.addEventListener('click', () => {
-    if(!navMenu.classList.contains('active')) {
-        navMenu.classList.add('active')
-    }
-})
+if (openBtn && closeBtn && navMenu) {
+    openBtn.addEventListener('click', () => {
+        if(!navMenu.classList.contains('active')) {
+            navMenu.classList.add('active')
+        }
+    })
 
-closeBtn.addEventListener('click', () => {
-    if(navMenu.classList.contains('active')) {
-        navMenu.classList.remove('active')
-    }
-})
+    closeBtn.addEventListener('click', () => {
+        if(navMenu.classList.contains('active')) {
+            navMenu.classList.remove('active')
+        }
+    })
+}
